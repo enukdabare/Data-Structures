cfcaf533d3ae8c861696d2922a020c3815bc90b0
Enuk Dabare <enukdabare@gmail.com>
2024-06-08 16:06:29 +0530

    Add files via upload

---
+++ FIFOQueueUsingArrayList.java
@@ -0,0 +1,10 @@
+package DataStructures;
+
+public class FIFOQueueUsingArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ FIFOQueueUsingLinkedList.java
@@ -0,0 +1,10 @@
+package DataStructures;
+
+public class FIFOQueueUsingLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
